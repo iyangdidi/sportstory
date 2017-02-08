cb2b125798f553250246ae36730bc7305a0677c5
iyangdidi <iyangdi@yeah.net>
2017-02-08 22:23:46 +0800

    URL统一管理测试

--- src/main/java/com/sportstory/controller/TestController.java
+++ src/main/java/com/sportstory/controller/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.sportstory.cons.UrlConstants;
 import com.sportstory.dao.TestDao;
 import com.sportstory.domain.User;
 import com.sportstory.service.UserService;
@@ -24,18 +25,18 @@ public class TestController {
 		
 		//private static final Logger log = Logger.getLogger(TestController.class);
 		
-		@RequestMapping("/")
+		@RequestMapping(UrlConstants.URE)
 		public void test(){
 			System.out.println("test...");
 		}
 	
-		@RequestMapping("/test/mybatis")
+		@RequestMapping(UrlConstants.URE_test_mybatis)
 	    public ModelAndView getIndex(){      
 	        ModelAndView mav = new ModelAndView("index");   
 	        return mav;    
 	    }   
 		
-		@RequestMapping("/test/jdbc")
+		@RequestMapping(UrlConstants.URE_test_jdbc)
 		public void testJDBC(){
 			testDao.getTestTableById();
 		}
